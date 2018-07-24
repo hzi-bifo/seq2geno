60bceb2a7764f40353d22347b26b6deb954e397e
thkuo <thkuo@ariel-compute-01.bifo.helmholtz-hzi.de>
2018-07-24 17:27:32 +0200

    quick cleaning

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#$ -V
-#$ -l h_core=16
-#$ -l h_vmem=60G
-
-cd /net/metagenomics/data/from_moni/old.tzuhao/seq2geno/dev_versions/v3
-source activate phypal
-snakemake  --snakefile=INTERFACE.smk
