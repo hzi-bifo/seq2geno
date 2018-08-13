27c9cb96808784c32bbacfb5517d850e2434d849
thkuo <thkuo@ariel-compute-01.bifo.helmholtz-hzi.de>
2018-08-13 12:03:26 +0200

    shell-free method to conduct vcf2indel.py

--- indel_detection/vcf2indel.py
+++ indel_detection/vcf2indel.py
@@ -1,6 +1,23 @@
+'''
+Author:
+    Original version by A. Weimann
+    Revisions by T.H. Kuo
+
+Main updates:
+    input directly from snakemake
+    python3-based
+'''
+
 import vcf as VCF
 import pandas as pd
-def vcf2indel(vcf, gene_name, out, out_gff, out_stats):
+def vcf2indel(args):
+    vcf=args.input['FAM_VCF']
+    gene_name=args.wildcards['fam']
+    out=args.output['FAM_INDELS_TXT']
+    out_gff=args.output['FAM_INDELS_GFF']
+    out_stats=args.output['FAM_INDELS_STATS']
+    len_cutoff=args.params['len_cutoff'] 
+
     no_indels = 0
     uq_indels = 0
     inserts = 0
@@ -12,33 +29,24 @@ def vcf2indel(vcf, gene_name, out, out_gff, out_stats):
             if not l.startswith("##"):
                 break 
         #read header
+        l=l.strip()
         columns = l.strip("#").split("\t")
         _, _, _, ref, alt, _, _, _, _ = columns[:9]
-        sample_names = pd.Series(["_".join(i.split(" ")[0].split("_")[:-1]) for i in columns[9:]])
+        sample_names = pd.Series([str(i) for i in columns[9:]])
         sample_has_indel = dict([(i, 1) for i in sample_names])
         #read and process variants
         for l in vcf_open:
             columns = pd.Series(l.strip().split("\t"))
             _, pos, _, ref, alt, _, _, _, _ = columns[:9]
-#### rewrite the above: pd.read_csv(roary_gpa_table, header= None, index_col=
-#### None, sep= '\t', comment= '#')            
             #insertion at beginning of gene
             hits_temp = {}
-            #the allele of every strain
             samples = pd.Series([i.split("/")[0] for i in columns[9:]])
             #compare length of ref and alternative alleles to extract indels
             alts = alt.split(",")
             #begin counting alt alleles from 1  reference allele  = 0
             for i, alt in zip(range(1, len(alts) + 1), alts) :
                 if not alt == "":
-                    
-#### What's this...?
-#### The character of deletion is also treated like one nucleotide
-                    if abs(len(alt) - len(ref)) >= 8:
-#### Only those present in half the strains are included
-#### The detection is NOT reference-based but MOJORITY-based
-#### No matter the deletion (.) was found in REF or ALT,
-#### the mutant type is always the monority one
+                    if abs(len(alt) - len(ref)) >= len_cutoff:
                         if ref == ".":
                             if len(sample_names.loc[samples == "."]) > sample_names.shape[0]/2:
                                 hits = sample_names.loc[samples != "."]
@@ -51,14 +59,9 @@ def vcf2indel(vcf, gene_name, out, out_gff, out_stats):
                                 hits = sample_names.loc[samples == "."]
                         #reference allele is "." but covered by at least one insertion and deletion
                         elif len(sample_names.loc[samples == "."]) > sample_names.shape[0]/2:
-                            # the inserted nucleotide
-                            # similar to the idea about putting each ALT allele
-                            # in one row
                             hits = sample_names.loc[(str(i) == samples) & (samples != ".")]
                         else:
-                            # When the ALT or REF is not an undetectable
-                            # character, no majority rule is applied...
-                            print i
+                            print(i)
                             hits = sample_names.loc[(str(i) == samples) | (samples == ".")]
                         if len(hits) != 0:
                             uq_indels += 1
@@ -70,7 +73,7 @@ def vcf2indel(vcf, gene_name, out, out_gff, out_stats):
                             if hit in hits_temp:
                                 if len(alt) > len(hits_temp[hit][2]):
                                     hits_temp[hit] = (hit, ref, alt, pos)
-                                    print hit, alt, "replace"
+                                    print(hit, alt, "replace")
                             else:
                                 hits_temp[hit] = (hit, ref, alt, pos)
                             sample_has_indel[hit] = 0 
@@ -90,12 +93,4 @@ def vcf2indel(vcf, gene_name, out, out_gff, out_stats):
     
 
 if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser("parse vcf and detect indels per sample")
-    parser.add_argument("vcf", help="vcf produced by msa2vcf")
-    parser.add_argument("gene_name", help="name of target gene e.g. ampD")
-    parser.add_argument("out", help="sample2indel")
-    parser.add_argument("out_gff", help="bed file with indel position")
-    parser.add_argument("out_stats", help="sample2indel")
-    args = parser.parse_args()
-    vcf2indel(**vars(args))
+    vcf2indel(snakemake)
