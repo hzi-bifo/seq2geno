843a5cf2f2de2a1c3760cda777252058632c9d15
thkuo <thkuo@ariel-compute-01.bifo.helmholtz-hzi.de>
2018-08-22 11:15:44 +0200

    restore Ariane's var calling to snps tables creation

--- command.sh
+++ command.sh
@@ -3,6 +3,6 @@
 #$ -l h_core=16
 #$ -l h_vmem=60G
 
-cd /net/metagenomics/data/from_moni/old.tzuhao/seq2geno/dev_versions/v4 
+cd /net/metagenomics/data/from_moni/old.tzuhao/seq2geno/dev_versions/v5
 source activate seq2geno
 snakemake --notemp --snakefile=INTERFACE.smk
