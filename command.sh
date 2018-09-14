4bbeb1c847cbb0b84ca534c810e16059fd2e04ab
thkuo <thkuo@ariel-compute-01.bifo.helmholtz-hzi.de>
2018-09-14 18:07:46 +0200

    create multi-sample vcf file with freebayes

--- command.sh
+++ command.sh
@@ -3,8 +3,7 @@
 #$ -l h_core=16
 #$ -l h_vmem=60G
 
-cd /net/metagenomics/data/from_moni/old.tzuhao/seq2geno/dev_versions/v5
-source activate seq2geno
+cd /net/metagenomics/data/from_moni/old.tzuhao/seq2geno/dev_versions/v7
+source activate ng_seq2geno
 
-snakemake --unlock --snakefile=MAIN.smk
-snakemake --notemp --snakefile=MAIN.smk
+snakemake --notemp --snakefile=test.smk
