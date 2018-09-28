4b04466c471dc5e109d07a7d8b7b758d3ef5fdaf
Tzu-Hao Kuo <tku16@helmholtz-hzi.de>
2018-09-28 17:15:38 +0200

    solve conflict

--- command.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#$ -V
-#$ -l h_core=16
-#$ -l h_vmem=80G
-
-cd /net/metagenomics/data/from_moni/old.tzuhao/seq2geno/dev_versions/v7
-source activate ng_seq2geno
-
-snakemake --notemp --snakefile=test.smk
