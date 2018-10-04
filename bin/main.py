f88276d1b854b36af80dc5d32b8a04b6d7aceff3
thkuo <thkuo@ariel-compute-01.bifo.helmholtz-hzi.de>
2018-10-04 10:53:02 +0200

    ng expr quantification automatically detecting single/paired reads

--- bin/main.py
+++ bin/main.py
@@ -14,12 +14,14 @@ if __name__== '__main__':
                 os.pardir))
     seq2geno_smk_dir = os.path.join(seq2geno_home, 'smk')
     seq2geno_lib_dir = os.path.join(seq2geno_home, 'lib')
+    seq2geno_env_dir = os.path.join(seq2geno_home, 'env')
 
     # parse user's arguments
     import UserOptions
     args= UserOptions.main()
     setattr(args, 'seq2geno_smk_dir', seq2geno_smk_dir)
     setattr(args, 'seq2geno_lib_dir', seq2geno_lib_dir)
+    setattr(args, 'seq2geno_env_dir', seq2geno_lib_dir)
 
     # create the config file
     config_f= 'test.yaml'
@@ -28,12 +30,7 @@ if __name__== '__main__':
 
     # Determine which version to use (ori or ng)
     main_smk= os.path.join(seq2geno_smk_dir, 
-            ('ng_test.smk' if args.ng else 'MAIN.smk'))
-    print(main_smk)
-    '''
-    main_smk= os.path.join(seq2geno_smk_dir, 
             ('ng_MAIN.smk' if args.ng else 'MAIN.smk'))
-    '''
 
     # run the workflow
     import snakemake
