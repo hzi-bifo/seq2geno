49da9ba85613bd20823627bb545d1c27d800c120
thkuo <thkuo@titan-compute-01.bifo.helmholtz-hzi.de>
2021-06-16 15:56:46 +0200

    fix logger

--- main/LogGenerator.py
+++ main/LogGenerator.py
@@ -28,5 +28,5 @@ def make_logger(log_f=''):
         log_fh = logging.FileHandler(filename=log_f)
         log_fh.setLevel(logging.DEBUG)
         log_fh.setFormatter(formatter)
-    logger.addHandler(log_fh)
+        logger.addHandler(log_fh)
     return(logger)
